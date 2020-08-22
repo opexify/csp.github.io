fddc98533e0815fd61b95c306225d14f7c93dc70
opexify <70063667+opexify@users.noreply.github.com>
2020-08-22 16:06:10 +0200

    Build for the CSP

---
+++ precache-manifest.2bbeccb9bda6ecd38ac88ba995da609d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bebfd07b3807b3d2a29ca0805e6cd227",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d78220ecdaba1edb8a2",
+    "url": "/static/css/main.6729addd.chunk.css"
+  },
+  {
+    "revision": "d64ab44b89b0db36696b",
+    "url": "/static/js/2.06c7b631.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.06c7b631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d78220ecdaba1edb8a2",
+    "url": "/static/js/main.ac7b16a1.chunk.js"
+  },
+  {
+    "revision": "e027e6ca5761f71d34c0",
+    "url": "/static/js/runtime-main.35cdfcaa.js"
+  },
+  {
+    "revision": "8dbcb5d5361e6f7cb148a457973347f6",
+    "url": "/static/media/marten-bjork-unsplash.8dbcb5d5.jpg"
+  },
+  {
+    "revision": "8b23b297e94508a7706f497a78bbd692",
+    "url": "/static/media/upya-logo-small.8b23b297.png"
+  }
+]);
